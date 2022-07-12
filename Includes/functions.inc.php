d9a86be6732a2a1357ead89f431a5b57def9b72e
edemadavid <edema.davidovie@gmail.com>
2022-07-12 08:07:39 +0100

    done corrections

--- Includes/functions.inc.php
+++ Includes/functions.inc.php
@@ -40,18 +40,20 @@ function pwdMatch($password, $pwdrepeat)  {
 function usernameExist($conn, $username, $email)  {
     $sql = "SELECT * FROM users WHERE Usernames = ? OR Useremails = ?;";
     $stmt = mysqli_stmt_init($conn);
-    if(mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../Main/SignUp.php?error=usernameoremailalreadyexist");
-        exit(); 
-    }
+    // if(mysqli_stmt_prepare($stmt, $sql)) {
+    //     header("location: ../Main/SignUp.php?error=usernameoremailalreadyexist");
+    //     exit(); 
+    // }
+    mysqli_stmt_prepare($stmt, $sql);
 
     mysqli_stmt_bind_param($stmt, "ss", $username, $email);
     mysqli_stmt_execute($stmt);
 
     $resultData = mysqli_stmt_get_result($stmt);
 
-    if($row = mysqli_fetch_assoc($resultData)) {
-        return $row;
+    if(mysqli_fetch_assoc($resultData)) {
+        header("location: ../Main/SignUp.php?error=usernameoremailalreadyexist");
+        exit();
     }
     else{
         $result = false;
@@ -64,11 +66,11 @@ function usernameExist($conn, $username, $email)  {
 function createUser($conn, $username, $email, $password)  {
     $sql = "INSERT INTO users (`Usernames`, `Useremails`, `Password` ) VALUES (?, ?, ?);";
     $stmt = mysqli_stmt_init($conn);
-    if(mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../Main/SignUp.php?error=stmtfailed");
-        exit(); 
-    }
-
+    // if(mysqli_stmt_prepare($stmt, $sql)) {
+    //     header("location: ../Main/SignUp.php?error=stmtfailed");
+    //     exit(); 
+    // }
+    mysqli_stmt_prepare($stmt, $sql);
     mysqli_stmt_bind_param($stmt, "sss", $username, $email, $password);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
