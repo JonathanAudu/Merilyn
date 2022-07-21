31a92d1d9a4e4a9cb39e66eb5af6a2d0e3c376d5
Jonathanaudu007 <jaudu2@gmail.com>
2022-07-21 22:48:16 +0100

    updates

--- Includes/updates.inc.php
+++ Includes/updates.inc.php
@@ -1,16 +1,14 @@
 <?php
-include_once "dbh.inc.php";
-if(isset($_POST["update"])) {
-    $id = $_POST["user_id"];
-    $firstname = $_POST["fname"];
-    $lastname = $_POST["lname"];
-    $desc = $_POST["description"];
-    $pnumber = $_POST["phone"];
-    $Haddress = $_POST["address"];
-    $Github = $_POST["github"];
-    $twitter = $_POST["twitter"];
-    $facebook = $_POST["facebook"];
+session_start();
+if(isset($_POST["update"])){
 
-
-    $stmt = "UPDATE userprofile SET 'first_name = '$firstname', last_name = '$lastname', `description` = '$' "
-}
+     $firstname = $_POST["fname"];
+     $lastname = $_POST["lname"];
+     $desc = $_POST["description"];
+     $pnumber = $_POST["phone"];
+     $Haddress = $_POST["address"];
+     $Github = $_POST["github"];
+     $twitter = $_POST["twitter"];
+     $facebook = $_POST["facebook"];
+    //  $profileimage = $_FILES["userimage"];
+} 
